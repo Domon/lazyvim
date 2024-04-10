8e28d1f28fa6d0848c2d4b1fd866ed5d7347d295
Chun-wei Kuo <Dendoh@gmail.com>
2024-04-10 16:42:26 +1000

    Config Neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+if vim.g.neovide then
+  vim.o.guifont = "AnonymicePro Nerd Font:h18"
+  vim.o.linespace = 25
+end
