d6edb07bd5490df2a13fa85f6289943d8804ce3c
Chun-wei Kuo <Dendoh@gmail.com>
2024-06-18 16:39:25 +1000

    Roll back to the default theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
   -- Install Solarized
-  { "maxmx03/solarized.nvim" },
+  -- { "maxmx03/solarized.nvim" },
 
   -- Configure LazyVim to load Solarized
-  {
-    "LazyVim/LazyVim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      background = "dark",
-      colorscheme = "solarized",
-    },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     background = "dark",
+  --     colorscheme = "solarized",
+  --   },
+  -- },
 }
