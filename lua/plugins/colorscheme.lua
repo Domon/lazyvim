b05e549cc83d95f305f2fae33df967a3e0387988
Chun-wei Kuo <Dendoh@gmail.com>
2024-06-27 17:51:31 +1000

    Switch to the Kanagawa Wave theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,4 +12,34 @@ return {
   --     colorscheme = "solarized",
   --   },
   -- },
+
+  -- Install Kanagawa
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim to load Kanagawa
+  {
+    "LazyVim/LazyVim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- colorscheme = "kanagawa", -- default
+      colorscheme = "kanagawa-wave", -- dark
+      -- colorscheme = "kanagawa-dragon", -- dimmed dark
+      -- colorscheme = "kanagawa-lotus", -- light
+    },
+  },
+
+  -- Install TokyoNight
+  -- { "folke/tokyonight.nvim" },
+
+  -- Configure LazyVim to load TokyoNight
+  -- {
+  --   "LazyVim/LazyVim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     colorscheme = "tokyonight",
+  --     style = "moon",
+  --   },
+  -- },
 }
