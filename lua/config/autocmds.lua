54df352e573f3ca23b96aaf78b24f0b984168bc0
Chun-wei Kuo <Dendoh@gmail.com>
2024-06-29 13:19:27 +1000

    Restore the cursor style when Vim is closed

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Restore the cursor style when Vim is closed
+vim.api.nvim_create_autocmd("VimLeave", {
+  group = vim.api.nvim_create_augroup("domon_restore_cursor_style_on_exit", { clear = true }),
+  callback = function()
+    -- A non-blinking vertical bar
+    --
+    --   * a        = all modes
+    --   * ver100   = vertical bar (100% height)
+    --   * blinkon0 = No blinking (0 ms)
+    --
+    -- See `:help guicursor` for more information.
+    vim.cmd("set guicursor=a:ver100-blinkon0")
+  end,
+})
