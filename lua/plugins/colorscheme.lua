72adeea2f70b846297bbbeda6d00146781b8860c
Chun-wei Kuo <Dendoh@gmail.com>
2024-06-29 13:57:53 +1000

    Install more themes to try

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,45 +1,37 @@
 return {
-  -- Install Solarized
-  -- { "maxmx03/solarized.nvim" },
-
-  -- Configure LazyVim to load Solarized
-  -- {
-  --   "LazyVim/LazyVim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     background = "dark",
-  --     colorscheme = "solarized",
-  --   },
-  -- },
-
-  -- Install Kanagawa
+  -- Install themes
+  { "catppuccin/nvim" },
+  { "neanias/everforest-nvim" },
+  { "ellisonleao/gruvbox.nvim" },
   { "rebelot/kanagawa.nvim" },
+  { "shaunsingh/nord.nvim" },
+  { "rose-pine/neovim" },
+  { "maxmx03/solarized.nvim" },
+  { "folke/tokyonight.nvim" },
 
-  -- Configure LazyVim to load Kanagawa
+  -- Configure LazyVim to load the theme
   {
     "LazyVim/LazyVim",
     lazy = false,
     priority = 1000,
     opts = {
+      -- colorscheme = "catppuccin-macchiato",
+
+      -- background = "soft",
+      -- colorscheme = "everforest",
+
       -- colorscheme = "kanagawa", -- default
       colorscheme = "kanagawa-wave", -- dark
       -- colorscheme = "kanagawa-dragon", -- dimmed dark
       -- colorscheme = "kanagawa-lotus", -- light
+
+      -- background = "dark",
+      -- colorscheme = "solarized",
+
+      -- colorscheme = "tokyonight",
+      -- style = "moon",
+
+      -- colorscheme = "rose-pine-moon",
     },
   },
-
-  -- Install TokyoNight
-  -- { "folke/tokyonight.nvim" },
-
-  -- Configure LazyVim to load TokyoNight
-  -- {
-  --   "LazyVim/LazyVim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     colorscheme = "tokyonight",
-  --     style = "moon",
-  --   },
-  -- },
 }
