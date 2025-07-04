f1f4aef4046b0abf048101d07e8a3f50564367ad
Chun-wei Kuo <Dendoh@gmail.com>
2025-07-04 14:35:30 +1000

    Make keymaps with the Opt key work in Neovide on macOS

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,7 @@
 if vim.g.neovide then
   vim.o.guifont = "AnonymicePro Nerd Font:h18"
   vim.o.linespace = 13
+
+  -- Interpret <Opt-*> as <M-*> so that keymaps like <M-b>, <M-f> work
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
 end
