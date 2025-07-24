abe8119f5d28b28881d3e5440b3457a9b7b0e06f
Chun-wei Kuo <Dendoh@gmail.com>
2025-07-24 16:18:35 +1000

    Introduce a workaround for a Blink configuration bug in LazyVim

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,7 +1,17 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    -- https://cmp.saghen.dev/configuration/keymap.html
-    keymap = { preset = "super-tab" },
+    keymap = {
+      -- https://cmp.saghen.dev/configuration/keymap.html
+      preset = "super-tab",
+      -- NOTE: Workaround until the following bug in LazyVim is fixed.
+      --   * https://github.com/LazyVim/LazyVim/issues/6185#issuecomment-3026219392
+      --   * https://github.com/LazyVim/LazyVim/pull/6183
+      ["<Tab>"] = {
+        require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+        require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+        "fallback",
+      },
+    },
   },
 }
