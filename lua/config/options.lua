161a0706ce41c5b45d22e51b6fa4a1abdfae0468
Chun-wei Kuo <Dendoh@gmail.com>
2026-07-15 13:59:19 +1000

    Make sure the file finder search from the closet git root

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Search from the git repo root and then current working directory
+-- See :LazyRoot for more information.
+vim.g.root_spec = { ".git", "cwd" }
+
 if vim.g.neovide then
   vim.o.guifont = "AnonymicePro Nerd Font:h18"
   vim.o.linespace = 13
